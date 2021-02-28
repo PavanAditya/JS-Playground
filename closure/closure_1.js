d5dff3734eb235dca2e635c1bf2b2b30253be957
Pavan Aditya M S <msp.aditya99@gmail.com>
2021-03-01 01:49:52 +0530

    closuure updated

--- closure/closure_1.js
+++ closure/closure_1.js
@@ -1,13 +1,15 @@
 function showName(firstName, lastName) {
 
+    // var nameIntro = "Hi,"; // ? var dec here can be used inside the child func as it is init before return
     return makeFullName();
 
     function makeFullName() {
-        return nameIntro + firstName + " " + lastName;
+        return nameIntro + " " + firstName + " " + lastName;
     }
-    var nameIntro = "My name is ";
-
-
+    
+    var nameIntro = "Hi,"; // ? var dec here cannot be used inside the child func as it is init after return
+    
 }
-console.log(showName("Sachin", "Tendulkar"));
-
+console.log(showName("I am", "Pavan"));
+// ? OP: (Hi, I am Pavan) if var is init before return 
+// ? OP: (undefined I am Pavan) if var is init after return 
