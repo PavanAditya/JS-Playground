5a59c647026f253b0851c0762e54bae534365230
Pavan Aditya M S <msp.aditya99@gmail.com>
2021-03-10 00:57:15 +0530

    bubbling and capturing exp comments updated

--- event-bubbling-and-capturing/main.js
+++ event-bubbling-and-capturing/main.js
@@ -5,18 +5,23 @@ const useCapture = false;
 document.querySelector("#grandparent")
     .addEventListener('click', () => {
         console.log('GrandPa Calling!!!!')
-    }, useCapture);
+    }, useCapture); // ? Third Param (useCapture) is not mandatory. Even if none is passed it is considered as false.
 
 document.querySelector("#parent")
     .addEventListener('click', () => {
         console.log('Papa Calling!!!!')
-    }, useCapture);
+    }, useCapture); // ? Third Param (useCapture) is not mandatory. Even if none is passed it is considered as false.
 
 document.querySelector("#child")
     .addEventListener('click', () => {
         console.log('Dude Calling!!!!')
-    }, useCapture);
+    }, useCapture); // ? Third Param (useCapture) is not mandatory. Even if none is passed it is considered as false.
 
 
 // ! Event Bubbling: Capturing events from inner div to the outer div. (Bubble Format)
 // ! Event Capturing/Trickling: Capturing events from outer div to inner div. (Hirearchial Format)
+
+// TODO: Be it any propogation chosen (Bubbling or Capturing) Both the cycles are followed. Chosing our cycle (by passing the useCapture param value) we are basically setting up which one to be executed first
+// TODO: Check will be done whether the method is executed in the previous cycle or not and will execute the method accordingly in this cycle (will execute it only if not covered in the previous cycle.)
+
+// * If the Click is only to be associated with the child div or the div only where the click happened, e.stopPropogation() can be used for stopping the next hirerachical event calls in the cycle.
