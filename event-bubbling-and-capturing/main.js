37a3b72fb5d7017c20bae67c7f570b1f77dc73a4
Pavan Aditya M S <msp.aditya99@gmail.com>
2021-03-10 00:43:30 +0530

    event bubbling and capturing

---
+++ event-bubbling-and-capturing/main.js
@@ -0,0 +1,22 @@
+// ? If the useCapture is sent true to the eventListener, Event Capturing/Trickling will happen
+// ? If the useCapture is sent false (or no value is passed to the eventListener), Event Bubbling will happen
+const useCapture = false; 
+
+document.querySelector('#grandparent')
+.addEventListener('click', () => {
+    console.log('GrandPa Calling!!!!')
+}, useCapture);
+
+document.querySelector('#grandparent')
+.addEventListener('click', () => {
+    console.log('GrandPa Calling!!!!')
+}, useCapture);
+
+document.querySelector('#grandparent')
+.addEventListener('click', () => {
+    console.log('GrandPa Calling!!!!')
+}, useCapture);
+
+
+// ! Event Bubbling: Capturing events from inner div to the outer div. (Bubble Format)
+// ! Event Capturing/Trickling: Capturing events from outer div to inner div. (Hirearchial Format)
