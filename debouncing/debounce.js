69020d7be8ac74c312e4a4d8db006f432035e561
Pavan Aditya M S <msp.aditya99@gmail.com>
2021-03-11 20:06:08 +0530

    debouncing and throttling explantion added

--- debouncing/debounce.js
+++ debouncing/debounce.js
@@ -17,3 +17,8 @@ let debounceData = (fn, timeout) => {
 };
 
 let getData = debounceData(fetchDataCall, 500);
+
+// ! DebounceData is called with a timer value. It initiates the timer with a timeOut of the give timer value.
+// ! If the function is called again before the timeOut gets completed the timer is resetted back to zero.
+// ! If the func call doesn't happen until the timeOut is over, then the fetchDataCall method (passed as an arg the the debounceData method) will be called and will execute the required functionality
+// ? This concept is know as debouncing. 
--- throttling/throttling.js
+++ throttling/throttling.js
@@ -22,3 +22,10 @@ const throtleName = (fn, timeout) => {
 }
 
 const showMyName = throtleName(myName, 500);
+
+// ! throtleName func sends a timeOut value along with the required function.
+// ! The throtleName func initially has a throtleFlag set to true. If the flag is true func passed as an argument will be called and our required func is executed.
+// ! Once the function is executed the throtleFalg becomes false and a timer with a given timeOut value is set to make it true again.
+// ! So even if the function is called again before the timer is completed, the function execution call will not happen as the flag is lying as false.
+// ! After the timeOut the flag becomes true and will be ready for another function call.
+// ? This concept is known as Throttling.
