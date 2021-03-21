12707f2cf522414272978ce7919b7ef93a8fc9b2
Pavan Aditya M S <msp.aditya99@gmail.com>
2021-03-22 00:24:15 +0530

    commments updated for forking

--- forking/child.js
+++ forking/child.js
@@ -1,11 +1,9 @@
-process.on('message', (msg) => {
-    console.log('Child:', msg);
-});
+process.on('message', (msg) => console.log('Child:', msg)); // ? Will listen to any msgs coming from the parent process.
 
 let counter = 1;
 setInterval(() => {
-    process.send(`count: ${counter++}`);
+    process.send(`count: ${counter++}`); // ? Sends/Emits the msg to the parent component. 
     if (counter === 5) {
-        process.exit(1);
+        process.exit(1); // ? Exits the process and emits the exit event (listened by the parent) to terminate the processing of the child function.
     }
 }, 1000);
--- forking/main.js
+++ forking/main.js
@@ -10,10 +10,10 @@ const fork = cp.fork;
 
 // app.listen(3000);
 
-const newChild = fork('forking/child.js');
+const newChild = fork('forking/child.js'); // ? A new instance of the child file is created. The child.js file will get executed under a new nodeJs main thread.
 
-newChild.send('Parent sent the code - 313.');
-newChild.on('message', (msg) => console.log('Parent:', msg));
-newChild.on('exit', (msg) => console.log('Exitted with msg', msg));
+newChild.send('Parent sent the code - 313.'); // ? Msg to the child process is sent/emitted using the send() method.
+newChild.on('message', (msg) => console.log('Parent:', msg)); // ? Any msgs coming form the child process will be caught here and will be executed on it.
+newChild.on('exit', (msg) => console.log('Exitted with msg', msg)); // ? If the chuld process emits the exit event, it is caught here and will stop the termination of the child process.
 
-console.log('Child pid:', newChild.pid);
+console.log('Child pid:', newChild.pid); // ? Will print the Process ID of the chuld process. 
