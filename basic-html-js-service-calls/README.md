aedcd3a77f1e40f0227c70edc0bcaf6f18e26b87
Pavan Aditya M S <msp.aditya99@gmail.com>
2022-01-19 01:30:13 +0530

    basic html/js service calls & new closure ex added

---
+++ basic-html-js-service-calls/README.md
@@ -0,0 +1,9 @@
+# All Possible HTML/JS Service Call Methods
+
+Methods Used:
+- XMLHttpRequest
+- Fetch
+- Axios
+- JQuery (AJAX)
+
+Open Api Json website used is [jsonplaceholder.typicode.com](jsonplaceholder.typicode.com)
--- closure/closure_1.js
+++ closure/closure_1.js
@@ -1,3 +1,5 @@
+// ! Example 1
+
 function showName(firstName, lastName) {
 
     // var nameIntro = "Hi,"; // ? var dec here can be used inside the child func as it is init before return
@@ -13,3 +15,26 @@ function showName(firstName, lastName) {
 console.log(showName("I am", "Pavan"));
 // ? OP: (Hi, I am Pavan) if var is init before return 
 // ? OP: (undefined I am Pavan) if var is init after return 
+
+
+// ! Example 2
+
+function x() {
+    var a = 1;
+    return function y() {
+        var b = 2;
+        return function z() {
+            var c = 3;
+            console.log(a, b, c); // ?  1 2 3
+        }
+    }
+};
+
+let z = x;
+z()()();
+// ? Since x is a function & it returns a function, with a nested return function, it is invoked thrice.
+// ?  It is similar to the following:
+// ?  let z1 = z();
+// ?  let z2 = z1();
+// ?  let z3 = z2();
+// ? OP: 1 2 3
